98176694ddb6aae2d7e97cae04bdffe617d8967d
ELAVARASAN P <elavarasan.p2023cse@sece.ac.in>
2025-07-08 09:30:20 +0530

    Day 4 AF

--- src/main/java/com/example/demo/ToDoApplication.java
+++ src/main/java/com/example/demo/ToDoApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootfirstApplication {
+public class ToDoApplication {
 
-	public static void main(String[] args) {
-
-		SpringApplication.run(SpringbootfirstApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/demo/repository/EmployeeRepository.java
+++ src/main/java/com/example/demo/repository/EmployeeRepository.java
@@ -1,9 +1,19 @@
 package com.example.demo.repository;
 
 import com.example.demo.Modules.Employee;
-import org.springframework.data.Jpa.repository.JpaRepository;
+import org.springframework.Jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findAll();
+
+    void save(Employee employee);
+
+    void deleteById(int id);
+
+    Optional<Object> findById(int id);
 }
